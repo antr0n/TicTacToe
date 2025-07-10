739975a885da5d9f0b1340dd6a31945bd0bd2dce
antr0n <tylerjanton@gmail.com>
2025-07-10 14:43:32 -0700

    fix: prevent overriding previous square X/O state on click

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) { return; }
     const nextSquares = squares.slice();
     xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
     setSquares(nextSquares);
